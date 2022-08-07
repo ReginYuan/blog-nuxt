8547b8b882bfa1be62e9f8765cf93ebbfadadb49
ReginYuan <2496943860@qq.com>
2022-08-07 15:32:19 +0800

    feat:代码修改

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,10 +67,6 @@ module.exports = {
   },
   // 注册插件
   plugins: ["~/plugins/request.js", "~/plugins/dayjs.js"],
-  server: {
-    host: "0.0.0.0",
-    port: 3000
-  },
   proxy: {
     "/api": {
       target: "http://localhost:3000"
